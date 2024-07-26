b2078e643c8b131830128a8108014dc70632c108
Mohammed Zaheer <152991044+MohammedZaheer0@users.noreply.github.com>
2024-07-26 11:56:38 +0530

    changed

--- app/components/CategoriesData.js
+++ app/components/CategoriesData.js
@@ -1,37 +1,3 @@
-// import React from 'react';
-// import Image from 'next/image';
-// import Category1 from '../../../public/images/categories1.png';
-// import Category2 from '../../../public/images/categories2.png';
-// import CategoriesAPI from '@/app/components/CategoriesAPI';
-
-// const Categories = () => {
-//   return (
-//     <div>
-//       <div className='w-[90%] mx-auto my-10 grid md:grid-cols-2 gap-10'>
-//         <div>
-//           <Image src={Category1} alt='Category1' className='w-[100%]' priority />
-//           <h1 className='text-[25px] font-medium my-10'>
-//             The Place Of <span className='text-[#FB7902]'>Beauty</span>
-//           </h1>
-//         </div>
-//         <div>
-//           <Image src={Category2} alt='Category2' className='w-[100%]' priority />
-//           <p className='text-[#4A4A4A] my-10'>
-//             Explore our makeup types, choose the one you like and follow the link to book,
-//             <br /> we’ll be glad to have you.
-//           </p>
-//         </div>
-//       </div>
-//       <CategoriesAPI />
-//     </div>
-//   );
-// };
-
-// export default Categories;
-
-
-
-
 const categories = [
     {
       heading: "Everyday Makeup",
--- app/page.js
+++ app/page.js
@@ -1,42 +1,13 @@
 import React from 'react'
 import Home from './components/Home';
-// import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
-// import ReactQuery from './reactQuery/page';
 
 const page = () => {
 
-  // const queryClient = new QueryClient();
-
   return (
-    // <QueryClientProvider client={queryClient}>
     <div>
       <Home/> 
-    </div>
-      
-      // <ReactQuery/>
-    // </QueryClientProvider>
+    </div>      
   )
 }
 
 export default page
-
-
-
-// import React from 'react';
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// import ReactQuery from './reactQuery/page'; // Import the component
-
-// const queryClient = new QueryClient();
-
-// const MainPage = () => {
-//     return (
-//         <QueryClientProvider client={queryClient}>
-//             <ReactQuery />
-//         </QueryClientProvider>
-//     );
-// };
-
-// export default MainPage;
-
-
-
